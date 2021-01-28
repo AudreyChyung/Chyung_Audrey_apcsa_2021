9e8a98849b9228f197728cb383acf1ff34f184cd
AudreyChyung <chyung.audrey@gmail.com>
2021-01-28 15:53:13 -0800

    Update AsciiArt.java

--- Unit00/src/AsciiArt.java
+++ Unit00/src/AsciiArt.java
@@ -1,24 +1,29 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
+//Name - Audrey Chyung
+//Date - 1/28/2021
+//Class - 
 //Lab  -
 
 public class AsciiArt
 {
 	public static void main ( String[] args )
 	{
-		System.out.println("Your Name \n\n" );
-		System.out.println("What type of ANIMAL YOU WILL DRAW" );
+		System.out.println("Your Name: Audrey Chyung \n\n" );
+		System.out.println("What type of ANIMAL YOU WILL DRAW: Cat" );
 		System.out.println("\n\n\n\n" );
 
-		System.out.println("                /\\                " );
-		System.out.println("               /  \\               " );
-		System.out.println("              /    \\              " );
-		System.out.println("             [------]             " );
-		//add other output
+		System.out.println("                /\\                     /\\                " );
+		System.out.println("               /  \\                   /  \\               " );
+		System.out.println("              /    \\_________________/    \\              " );
+		System.out.println("             |------                 ------|             " );
+		System.out.println("            /                               \\             " );
+		System.out.println("           |       O              O          |             " );
+		System.out.println("           |                                 |             " );
+		System.out.println("           |  ==          ^          ==      |             " );
+		System.out.println("            \\                               /             " );
+		System.out.println("             \\_____________________________/             " );
 
 		System.out.println(" \n\n\n\nHelpFul Hints" );
 		System.out.println("\\\\ draws one backslash on the screen!\n" );
