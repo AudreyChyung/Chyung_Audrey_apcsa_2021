428c4f9153ddfc6306eb51c8b392414c376056a0
AudreyChyung <chyung.audrey@gmail.com>
2021-01-29 12:51:27 -0800

    StarsAndStripes lab

--- Unit01/src/StarsAndStripes.java
+++ Unit01/src/StarsAndStripes.java
@@ -18,21 +18,38 @@ public class StarsAndStripes
 
    public void printTwentyStars()
    {
+	   out.println("********************");
    }
 
    public void printTwentyDashes()
    {
+	   out.println("--------------------");
    }
 
    public void printTwoBlankLines()
    {
+	   out.println("\n");
    }
    
    public void printASmallBox()
    {	
+	   printTwentyDashes();
+	   printTwentyStars();
+	   printTwentyDashes();
+	   printTwentyStars();
+	   printTwentyDashes();
    }
  
    public void printABigBox()
    { 	
+	   printTwentyDashes();
+	   printTwentyStars();
+	   printTwentyDashes();
+	   printTwentyStars();
+	   printTwentyDashes();
+	   printTwentyStars();
+	   printTwentyDashes();
+	   printTwentyStars();
+	   printTwentyDashes();
    }   
 }
