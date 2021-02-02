6b23230c7f58fd390b26df29693ebacb63048263
AudreyChyung <chyung.audrey@gmail.com>
2021-02-02 12:11:47 -0800

    Update Line.java

--- Unit02/src/Line.java
+++ Unit02/src/Line.java
@@ -5,9 +5,10 @@
 
 public class Line
 {
-	public static double getSlope( double x1, double y1, double x2, double y2 )
+	public static double getSlope( int x1, int y1, int x2, int y2 )
 	{
-		return (y2-y1)/(x2-x1);
+		double dx1 = x1, dx2 = x2, dy1 = y1, dy2 = y2;
+		return (dy2-dy1)/(dx2-dx1);
 	}
 
 }
