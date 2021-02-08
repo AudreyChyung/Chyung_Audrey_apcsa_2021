651dea7b9ae6a7fc2124473151947adbb014ec41
AudreyChyung <chyung.audrey@gmail.com>
2021-02-08 12:29:58 -0800

    Letter Remover lab

--- Unit06/src/LetterRemover.java
+++ Unit06/src/LetterRemover.java
@@ -12,9 +12,13 @@ public class LetterRemover
 	public LetterRemover()
 	{
 		//call set
+		setRemover("",' ');
 	}
 
 	//add in second constructor
+	public LetterRemover(String s, char rem) {
+		setRemover(s, rem);
+	}
 	
 	
 	public void setRemover(String s, char rem)
@@ -26,11 +30,16 @@ public class LetterRemover
 	public String removeLetters()
 	{
 		String cleaned=sentence;
+		int loc = cleaned.indexOf(lookFor);
+		while(loc > -1) {
+			cleaned = cleaned.substring(0, loc) + cleaned.substring(loc + 1);
+			loc = cleaned.indexOf(lookFor);
+		}
 		return cleaned;
 	}
 
 	public String toString()
 	{
-		return sentence + " - letter to remove " + lookFor;
+		return sentence + " - letter to remove " + lookFor + "\n" + removeLetters() + "\n";
 	}
 }
