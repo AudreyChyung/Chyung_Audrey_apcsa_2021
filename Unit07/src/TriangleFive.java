18493d1251b29ecabc9abde8f9a1ce72373456e4
AudreyChyung <chyung.audrey@gmail.com>
2021-02-15 00:29:32 -0800

    Unit07 labs

--- Unit07/src/TriangleFive.java
+++ Unit07/src/TriangleFive.java
@@ -11,23 +11,48 @@ public class TriangleFive
 
 	public TriangleFive()
 	{
+		setLetter(' ');
+		setAmount(0);
 	}
 
 	public TriangleFive(char c, int amt)
 	{
+		setLetter(c);
+		setAmount(amt);
 	}
 
 	public void setLetter(char c)
 	{
+		letter = c;
 	}
 
 	public void setAmount(int amt)
 	{
+		amount = amt;
 	}
-
+	
 	public String toString()
 	{
 		String output="";
+		
+		for (int i = 0; i < amount; i++) {
+			char tempLetter = letter;
+			for (int j = 0; j < amount - i; j++) {
+				for (int k = 0; k < amount - j; k++) {
+					output += tempLetter;
+				}
+				output += " ";
+				
+				//get the next letter, wrapping around to A if the letter is Z
+				if (tempLetter == 'Z')
+					tempLetter = 'A';
+				else 
+					tempLetter++;
+			}
+			output += '\n';
+		}
+		
 		return output;
 	}
 }
+	
--- Unit07/src/Triples.java
+++ Unit07/src/Triples.java
@@ -15,30 +15,45 @@ public class Triples
 
 	public Triples(int num)
 	{
-
-
+		setNum(num);
 	}
 
 	public void setNum(int num)
 	{
-
-
+		number = num;
 	}
 	
 	private int greatestCommonFactor(int a, int b, int c)
 	{
 		int max = 0;
-
-
-
-		return 1;
+		for (int i = 1; i < a; i++){
+		   if (a%i == 0 && b%i == 0 && c%i == 0)
+			   max = i;
+		}
+		
+		return max;
 	}
 
 	public String toString()
 	{
 		String output="";
 
-
+		for (int a = 1; a <= number; a++) {
+			for (int b = 1; b <= number; b++) {
+				for (int c = 1; c <= number; c++) {
+					//check if a, b, and c make a valid triple
+					if (Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2) &&
+						((a%2 == 0 && b%2 != 0) || (a%2 != 0 && b%2 == 0)) &&
+						c%2 != 0 &&
+						greatestCommonFactor(a, b, c) <= 1 &&
+						a < b &&
+						b < c) 
+					{
+						output = output + "\n" + a + " " + b + " " + c;
+					}
+				}
+			}
+		}
 
 
 
