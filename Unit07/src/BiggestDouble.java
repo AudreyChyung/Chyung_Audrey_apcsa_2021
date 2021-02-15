d8b8e5bdbf0f4fa619d5277a4970f40a0a3fbda5
AudreyChyung <chyung.audrey@gmail.com>
2021-02-14 16:59:12 -0800

    biggest double lab

--- Unit07/src/BiggestDouble.java
+++ Unit07/src/BiggestDouble.java
@@ -15,19 +15,31 @@ public class BiggestDouble
 
 	public BiggestDouble(double a, double b, double c, double d)
 	{
+		setDoubles(a,b,c,d);
 	}
 
 	public void setDoubles(double a, double b, double c, double d)
 	{
+		one = a;
+		two = b;
+		three = c;
+		four = d;
 	}
 
 	public double getBiggest()
 	{
-		return 0.0;
+		double biggest = one;
+		if (two > biggest)
+			biggest = two;
+		if (three > biggest)
+			biggest = three;
+		if (four > biggest)
+			biggest = four;
+		return biggest;
 	}
 
 	public String toString()
 	{
-	   return "";
+	   return one + " " + two + " " + three + " " + four;
 	}
 }
