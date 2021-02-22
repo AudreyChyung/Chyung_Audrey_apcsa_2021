8d7894bf4b411dcf5072ff16941fcf1fa1ef6830
AudreyChyung <chyung.audrey@gmail.com>
2021-02-21 20:17:07 -0800

    update card testers

--- Unit09/src/activity1/CardTester.java
+++ Unit09/src/activity1/CardTester.java
@@ -1,5 +1,7 @@
 package activity1;
 
+import activity2.Card;
+
 /**
  * This is a class that tests the Card class.
  */
@@ -11,15 +13,15 @@ public class CardTester {
 	 */
 	public static void main(String[] args) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-		Card kingDiamonds1 = new Card("king", "diamonds", 10);
+		Card aceDiamonds1 = new Card("ace", "diamonds", 1);
 		Card fourSpades1 = new Card("4", "spades", 4);
 		Card fourSpades2 = new Card("4", "spades", 4);
 		
-		System.out.println("king of diamonds 1 tests");
-		System.out.println(kingDiamonds1.rank());
-		System.out.println(kingDiamonds1.suit());
-		System.out.println(kingDiamonds1.pointValue());
-		System.out.println(kingDiamonds1.toString());
+		System.out.println("ace of diamonds 1 tests");
+		System.out.println(aceDiamonds1.rank());
+		System.out.println(aceDiamonds1.suit());
+		System.out.println(aceDiamonds1.pointValue());
+		System.out.println(aceDiamonds1.toString());
 		
 		System.out.println("four of spades 1 tests");
 		System.out.println(fourSpades1.rank());
